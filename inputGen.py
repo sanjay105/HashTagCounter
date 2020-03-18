24db538c8d339f7a9b0a37722affe4e0b727d698
Sanjay Reddy <sanjayreddy.banda@gmail.com>
2020-03-18 01:21:11 -0400

    Input generation script file

---
+++ inputGen.py
@@ -0,0 +1,26 @@
+import random
+import sys
+orig_stdout = sys.stdout
+for i in range(1,7):
+  tot=10**i
+  fname="input_"+str(tot)+".txt"
+  f = open(fname, 'w')
+  sys.stdout = f
+  i=0
+  number=ord('a')
+  while(i!=tot):
+    x=random.randint(1,10)
+    if x!=10:
+      a=random.randint(1,100)
+      count=random.randint(1,20)
+      s='#'
+      for k in range(count):
+        b=random.randint(0,25)
+        s=s+chr(number+b)
+      print(s,a)
+    else:
+      print(random.randint(1,20))
+    i+=1
+  print("stop")
+  sys.stdout = orig_stdout
+  f.close()
